b4043a0efa4430ceb86156f03378f2d4757978a1
Tomoyuki Oho <oho_cooh@hotmail.com>
2018-07-22 16:35:25 +0900

    * 円を12個に変更

--- myscript.js
+++ myscript.js
@@ -5,6 +5,7 @@ var radius;
 function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
     background('white');
+    noStroke();
 }
 
 function draw() {
@@ -14,16 +15,17 @@ function draw() {
     background('white');
 
     radius = window.innerWidth / 3;
+    var r = canvas.width / 10;
 
     push();
     translate(canvas.width / 2, canvas.height / 2);
 
-    fill('pink');
-    noStroke();
-    var r = canvas.width / 10;
-    var x = sin(radians(angle)) * radius;
-    var y = cos(radians(angle)) * radius;
-    ellipse(x, y, r, r);
+    for(var i = 0; i < 12; i++) {
+        fill('pink');
+        var x = sin(radians(angle + (360 / 12 * i))) * radius;
+        var y = cos(radians(angle + (360 / 12 * i))) * radius;
+        ellipse(x, y, r, r);
+    }
 
     pop();
 
