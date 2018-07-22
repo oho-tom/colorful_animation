b96550baf6242c9ba80ee59c9502713619076f5b
Tomoyuki Oho <oho_cooh@hotmail.com>
2018-07-22 16:30:30 +0900

    * 円周運動するアニメーションを作成

--- myscript.js
+++ myscript.js
@@ -1,4 +1,6 @@
 var canvas;
+var angle = 0;
+var radius;
 
 function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
@@ -11,7 +13,19 @@ function draw() {
     canvas.height = window.innerHeight;
     background('white');
 
+    radius = window.innerWidth / 3;
+
+    push();
+    translate(canvas.width / 2, canvas.height / 2);
+
     fill('pink');
     noStroke();
-    ellipse(canvas.width / 2, canvas.height / 2, 10, 10);
+    var r = canvas.width / 10;
+    var x = sin(radians(angle)) * radius;
+    var y = cos(radians(angle)) * radius;
+    ellipse(x, y, r, r);
+
+    pop();
+
+    angle++;
 }
